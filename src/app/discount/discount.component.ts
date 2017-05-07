1b5b68aaa4f73fae8657eabe14827c812c78bf9a
曹力升 <c_youxin@163.com>
2017-05-07 20:27:25 +0800

    reduce duplicate codes.

--- src/app/discount/discount.component.ts
+++ src/app/discount/discount.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
     selector: 'app-discount',
@@ -9,4 +9,12 @@ export class DiscountComponent {
 
     @Input()
     show: boolean;
+
+    @Output()
+    btnClick = new EventEmitter();
+
+    btnClicked() {
+        this.btnClick.emit(Math.random() * 5);
+        this.show = false;
+    }
 }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-order',
     templateUrl: './order.component.html',
     styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit{
+export class OrderComponent implements OnInit {
 
     goodslist: Array<any>;
     message: string;
@@ -19,16 +20,33 @@ export class OrderComponent implements OnInit{
     freight = '32.00';
     discount = '1.00';
     isDiscountShow = new Promise(res => res(false));
+    isAddressShow = new Promise(res => res(false));
+
+    constructor(private router: Router) {}
 
     ngOnInit(): void {
         this.goodslist = JSON.parse(localStorage.getItem('order'));
         this.amount = '' + (this.goodslist.reduce((p, goods) => {
-            return p + parseFloat(goods.price);
-        }, 0) + parseFloat(this.freight)).toFixed(2);
+                return p + parseFloat(goods.price);
+            }, 0) + parseFloat(this.freight)).toFixed(2);
         this.totalAmount = '' + (Math.max(0, parseFloat(this.amount) - parseFloat(this.discount))).toFixed(2);
     }
 
     showDiscount() {
         this.isDiscountShow = new Promise(res => res(true));
+    }
+
+    refreshDiscount(e) {
+        this.discount = '' + e.toFixed(2);
+        this.totalAmount = '' + (Math.max(0, parseFloat(this.amount) - parseFloat(this.discount))).toFixed(2);
+    }
+
+    createOrder() {
+        alert('订单提交啦！！！');
+        const navigate = this.router.navigate(['/']);
+    }
+
+    showAddress() {
+        this.isAddressShow = new Promise(res => res(true));
     }
 }
